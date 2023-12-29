434d1793e4978bd28d14afe0242af32af1223b75
Daniel Aparicio <Daniel.Aparicio@niceincontact.com>
2023-12-29 15:46:06 -0400

    Remove unnecesary files

--- Modules/hello.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function helloWorld() {
-    console.log("Hello, world!");
-}
-exports.default = helloWorld;
--- Modules/test.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const hello_js_1 = require("./hello.js");
-(0, hello_js_1.default)();
